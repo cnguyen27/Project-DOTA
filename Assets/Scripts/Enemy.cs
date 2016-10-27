b0bc5f709135cfe1586d2e9859fe611b7a946ce1
clee117 <clee00321@gmail.com>
2016-10-26 19:53:56 -0700

    Enemy & Map Detection
    WIP scripts for enemy and map detection.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class Enemy: MonoBehaviour {
+	
+	public int health;
+	public bool activate;
+	
+	void Update{
+		if (activate == true) {
+			
+		}
+	}
+	
+}
